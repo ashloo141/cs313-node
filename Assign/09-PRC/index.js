b6d06c474705fdb0e3ee51d8489d0d335135f90b
ashloo141 <loo16001@byui.edu>
2019-11-17 00:11:49 -0700

    set up

--- Assign/09-PRC/index.js
+++ Assign/09-PRC/index.js
@@ -1,1 +1,10 @@
-hello world!
+const express = require('express');
+var app = express();
+
+app.set('port', process.env.PORT || 5000)
+//    .use(express.static(__dirname + '/public'))
+//    .set('views', __dirname + '/views')
+//    .set('view engine', 'ejs')
+   .listen(app.get('port'), function(){
+       console.log('Listening on port: ' + app.get('port'));
+   });
